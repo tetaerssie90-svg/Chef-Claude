a15bf6177159a78177bd72a93792475d05aab09d
tetaerssie90-svg <tetaerssie90@gmail.com>
2026-09-17 10:37:29 +0200

    Done with styling the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./Header"
+import Dot from "./dot"
+
 
 export default function App() {
   return (
+    <>
       <Header />
+      <Dot/>
+
+    </>
   )
 }
---
+++ src/Dot.jsx
@@ -0,0 +1,19 @@
+export default function Dot() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+
+                />
+                <button>Add ingredient</button>
+
+
+            </form>
+
+
+        </main>
+    )
+}
